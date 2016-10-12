7a3a858dc3d4943369635b8894bf7f2015e4040c
Domenico Fabio Marino <dfm114@ic.ac.uk>
2016-10-12 17:58:20 +0200

    Backend: Linter: handle input arguments, output execution errors
    Signed-off-by: Domenico Fabio Marino <dfm114@ic.ac.uk>

--- backend/linter/src/main/scala/linter/App.scala
+++ backend/linter/src/main/scala/linter/App.scala
@@ -1,37 +1,52 @@
 package linter
-import java.io.File
+
+import java.io.{File, FileInputStream, FileWriter}
 
 import linter.linters.{BaseLinter, LengthCheckerLinter}
 import play.api.libs.json._
 
 import scala.collection.mutable
+
 /**
- * @author ${user.name}
- */
+  * @author ${user.name}
+  */
 object App {
 
   private val lintersList = new mutable.MutableList[BaseLinter]
+  private val mistakes = new mutable.MutableList[LinterError]
+  private val executionErrors = new mutable.MutableList[String]
   private var language: Language.Value = Language.Other
   private var path: File = _
-  private val mistakes = new mutable.MutableList[LinterError]
-  def main(args : Array[String]) {
-    val json = Json.parse(System.in)
-    path = new File((json \ "path").as[String])
-    language = Language.matchString((json \ "language").as[String])
-    val list = (json \ "linters").asOpt[List[String]].get
-    list.foreach(parseLinters)
-    mistakes ++= lintersList.flatMap(_.parseFiles)
-    print("mistakes" -> Json.toJson(mistakes.map(_.toString)))
-    //linters.toArray().foreach(l => parseLinters(l.toString))
 
-    //val linter = new Linter(new File(path), Language.matchString(language))
-    //val out = Json.toJson("mistakes" -> )
-    //print(linter.parseFiles.toList)
+  def main(args: Array[String]) {
+    if (args.length != 2) {
+      throw new IllegalArgumentException(s"linter <input json> <output json>")
+    }
+    val inputReader = new FileInputStream(args.apply(0))
+    val json = Json.parse(inputReader)
+    path = new File((json \ "path").as[String])
+    val mLanguage = (json \ "language").asOpt[String]
+    language = Language.matchString(mLanguage.getOrElse("haskell"))
+    val list = (json \ "linters").asOpt[List[String]].getOrElse(Seq("baselinter"))
+    try {
+      list.foreach(parseLinters)
+      mistakes ++= lintersList.flatMap(_.parseFiles)
+    } catch {
+      case e: Exception => executionErrors += e.getMessage
+    }
+    val outputStream = new FileWriter(args.apply(1))
+
+    val result = Json.obj(
+      "mistakes" -> mistakes.map(_.toString),
+      "execution_errors" -> executionErrors.map(_.toString)
+    )
+    outputStream.write(Json.stringify(result))
+    outputStream.close()
   }
 
   private def parseLinters(str: String): Unit = str match {
     case "baselinter" => lintersList += new LengthCheckerLinter(path, language)
-    case _ => throw new NotImplementedError(s"Wrong linter $str")
+    case _ => executionErrors += s"No such linter <$str>"
   }
 
 
