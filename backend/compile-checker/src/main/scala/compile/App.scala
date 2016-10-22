cd2054e057b69574a097332eddbbd5333baf3a6b
Michael Carneciu <mc3714@ic.ac.uk>
2016-10-22 22:19:09 +0100

    Minor changes. need to output json

--- backend/compile-checker/src/main/scala/compile/App.scala
+++ backend/compile-checker/src/main/scala/compile/App.scala
@@ -3,17 +3,19 @@ package compile
 import java.io.{File, FileInputStream}
 
 import play.api.libs.json.Json
+import sun.reflect.generics.reflectiveObjects.NotImplementedException
 import sys.process._
 
 /**
-  * This program uses JSON as input and outputs 0 if the given
-  * <p> program compiles, or an integer otherwise.
+  * This program uses JSON as both input and output.
+  * <p> We return a score of 100 if the
+  * <p> program compiles, or a 0 otherwise, with the
+  * <p> corresponding errors associated to the compilation.
   * <p> Expected input format: <p>
   * <p> { </p>
   * <p>   "input_directory": "<path to a file>", </p>
   * <p>   "type": "<language to be used>", </p>
   * <p> } </p>
-  * <p> Language and linters are optional </p>
  */
 object App {
 
@@ -28,16 +30,18 @@ object App {
     path = new File((json \ "input_directory").as[String])
     val mLanguage = (json \ "type").asOpt[String]
     val language = mLanguage.getOrElse("haskell")
-    print(compileCheck(language, path))
+    /*TODO: tomorrow
+    if (compileCheck(language, path) == 0) {
+
+    } else {
+
+    }*/
   }
 
   private def checkHaskell(path: File) = {
-    var returnCode = 0
-    val haskellFiles = path.list().filter(_ endsWith ".hs")
-    // GHC output is generated; can be supressed
-    for (file <- haskellFiles) returnCode +=
-        s"ghc -i$path/IC -i$path $path/$file".!
-    returnCode
+    val haskellFiles = path.listFiles().filter(e
+        => e.getName.endsWith(".hs") || e.getName.endsWith(".lhs")).mkString(" ")
+    s"ghc -i$path/IC -i$path $haskellFiles" !
   }
 
   private def checkJava(path: File) = {
@@ -46,9 +50,9 @@ object App {
     s"javac $javaFiles" !
   }
 
-  private def compileCheck(str: String, path: File): Int = str match {
+  private def compileCheck(language: String, path: File): Int = language match {
     case "haskell" => checkHaskell(path)
     case "java" => checkJava(path)
-    case _ => 0
+    case _ => throw new NotImplementedException
   }
 }
