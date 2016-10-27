17ccd8490405d9de14851210db29a4e29bb2cc5e
dfm114 <dfm114@ic.ac.uk>
2016-10-27 13:46:27 +0100

    Backend: ASTComparator: Start impelementation in Groovy
    Signed-off-by: dfm114 <dfm114@ic.ac.uk>

---
+++ backend/ast_comparator/src/main/java/ast_comparator/Main.groovy
@@ -0,0 +1,16 @@
+package ast_comparator
+
+import org.antlr.v4.runtime.ANTLRInputStream
+
+/**
+ * Created by dfm114 on 27/10/16.
+ */
+class Main {
+
+    static void main(String... args) {
+        ANTLRInputStream stream = new ANTLRInputStream(System.in);
+        println stream.toString()
+    }
+
+
+}
