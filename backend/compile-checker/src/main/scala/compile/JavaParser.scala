e2b84ea60925049574e0d91251400059055d59bb
Mihai-Alexandru Carneciu <mihai-alexandru.carneciu14@imperial.ac.uk>
2016-10-28 17:22:50 +0100

    Finished implementation of reporting compile errors for Java

--- backend/compile-checker/src/main/scala/compile/JavaParser.scala
+++ backend/compile-checker/src/main/scala/compile/JavaParser.scala
@@ -8,23 +8,32 @@ import scala.sys.process._
 /**
   * Object used to compile java files and parse eventual errors
   */
-object JavaParser extends Parser {
-  private val pathMatch = "(.*?.java)".r
-  private val posMatch = s":$digitsMatch".r
 
-  // TODO: deal with java errors
-  override def process(lines: ArrayBuffer[String]): Seq[Error] = {
-    lines.foreach(println)
-    Seq()
+object JavaParser extends Parser {
+  private val pathMatch = "(.*.java)".r
+  private val posMatch = s":$digitsMatch".r
+  private val javaReasonMatch = ": (.*)".r
+
+  private def genAnnotations(error: String): Error = {
+    val path = pathMatch.findFirstIn(error).get
+    val positions = posMatch.findFirstIn(error).get
+    val row = digitsMatch.findFirstIn(positions).get.toInt
+    val errorReason = javaReasonMatch.findFirstIn(error).get.substring(2)
+    new Error(errorReason, path, row, 0, "compilation")
   }
 
+  override def process(lines: ArrayBuffer[String]): Seq[Error] = {
+    val errorMatch = s"$pathMatch$posMatch$javaReasonMatch".r
+    val text = lines.mkString("\n")
+    val errors = errorMatch.findAllIn(text)
+    errors.map(e => genAnnotations(e)).toSeq
+  }
 
   override def check(path: File) = {
     val javaFiles =
       path.listFiles().filter(e => e.getName.endsWith(".java")).mkString(" ")
     val lines = new ArrayBuffer[String]()
     val exit = s"javac $javaFiles" ! ProcessLogger(line => lines.append(line))
-
     (exit, process(lines))
   }
 }
