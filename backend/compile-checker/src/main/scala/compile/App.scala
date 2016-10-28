2e0e7b15573d8751acb324c08b3752568c1f6c96
Michael Carneciu <mc3714@ic.ac.uk>
2016-10-28 11:47:57 +0100

    Modified exit code & error logic for haskell. To implement the json input/output

--- backend/compile-checker/src/main/scala/compile/App.scala
+++ backend/compile-checker/src/main/scala/compile/App.scala
@@ -4,6 +4,7 @@ import java.io.{File, FileInputStream}
 
 import play.api.libs.json.Json
 import sun.reflect.generics.reflectiveObjects.NotImplementedException
+import scala.collection.mutable.ArrayBuffer
 import sys.process._
 
 /**
@@ -23,25 +24,26 @@ object App {
 
   def main(args : Array[String]) {
     if (args.length != 1) {
-      throw new IllegalArgumentException(s"Compile checker <input json> ")
+      throw new IllegalArgumentException("Compile checker <input json> ")
     }
+
+
     val inputReader = new FileInputStream(args.apply(0))
     val json = Json.parse(inputReader)
     path = new File((json \ "input_directory").as[String])
     val mLanguage = (json \ "type").asOpt[String]
     val language = mLanguage.getOrElse("haskell")
-    /*TODO: tomorrow
-    if (compileCheck(language, path) == 0) {
-
-    } else {
-
-    }*/
+    compileCheck(language, path).toString.isEmpty)
   }
 
   private def checkHaskell(path: File) = {
     val haskellFiles = path.listFiles().filter(e
         => e.getName.endsWith(".hs") || e.getName.endsWith(".lhs")).mkString(" ")
-    s"ghc -i$path/IC -i$path $haskellFiles" !
+    val lines = new ArrayBuffer[String]()
+    val exit = s"ghc -i$path/IC -i$path $haskellFiles" ! ProcessLogger(line => lines.append(line))
+    println(exit)
+    lines.tail.foreach(println)
+    0
   }
 
   private def checkJava(path: File) = {
