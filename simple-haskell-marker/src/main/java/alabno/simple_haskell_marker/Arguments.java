06178c306bc84418340f1543ae7e3ccb9dfdb572
Giulio <giuliojiang@gmail.com>
2016-11-12 16:22:06 +0000

    changing the Simple Haskell Marker to dynamically load most recent training data

--- simple-haskell-marker/src/main/java/alabno/simple_haskell_marker/Arguments.java
+++ simple-haskell-marker/src/main/java/alabno/simple_haskell_marker/Arguments.java
@@ -7,17 +7,20 @@ public class Arguments {
 
     private List<String> haskellInputs = new ArrayList<>();
     private String outputJsonPath;
+    private String trainingDataPath;
     
     public Arguments(String[] args) {
-        if (args == null || args.length < 1) {
-            System.out.println("Error: invalid command line arguments");
-            outputJsonPath = null;
-            return;
-        }
-        for (int i = 0; i < args.length - 1; i++) {
-            haskellInputs.add(args[i]);
-        }
-        outputJsonPath = args[args.length - 1];
+//        if (args == null || args.length < 1) {
+//            System.out.println("Error: invalid command line arguments");
+//            outputJsonPath = null;
+//            return;
+//        }
+//        for (int i = 0; i < args.length - 1; i++) {
+//            haskellInputs.add(args[i]);
+//        }
+//        outputJsonPath = args[args.length - 1];
+        
+        
     }
     
     public List<String> getHaskellInputs() {
