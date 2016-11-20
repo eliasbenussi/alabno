bb5148ae033a3f25a4ceb5f212fbb1eb9608b65b
giuliojiang <giuliojiang@gmail.com>
2016-11-20 20:53:39 +0000

    change default timeout value for mysql connection

--- infrastructure/infrastructure/src/main/java/alabno/database/MySqlDatabaseConnection.java
+++ infrastructure/infrastructure/src/main/java/alabno/database/MySqlDatabaseConnection.java
@@ -7,6 +7,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
+import java.sql.SQLTimeoutException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -19,198 +20,202 @@ import alabno.utils.FileUtils;
 
 public class MySqlDatabaseConnection {
 
-    // JDBC driver name and database URL
-    private String DB_URL = "jdbc:mysql://tc.jstudios.ovh:3306/Automarker";
+	// JDBC driver name and database URL
+	private String DB_URL = "jdbc:mysql://tc.jstudios.ovh:3306/Automarker";
 
-    // Database credentials
-    static final String USER = "python";
-    private String dbPassword = "";
+	// Database credentials
+	static final String USER = "python";
+	private String dbPassword = "";
 
-    private Connection conn;
+	private Connection conn;
 
-    public MySqlDatabaseConnection() {
-        setupPassword();
-        connect();
-    }
+	public MySqlDatabaseConnection() {
+		setupPassword();
+		connect();
+	}
 
-    private void setupPassword() {
-        String workDir = FileUtils.getWorkDir();
-        String passwordPath = workDir + "/dbpass.txt";
-        File passwordFile = new File(passwordPath);
-        Scanner scanner = null;
-        try {
-            scanner = new Scanner(passwordFile);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            System.out.println("Could not find the password file! Make sure to use RunServer");
-            System.exit(1);
-        }
-        String thePassword = scanner.nextLine();
-        scanner.close();
-        this.dbPassword = thePassword;
-    }
+	private void setupPassword() {
+		String workDir = FileUtils.getWorkDir();
+		String passwordPath = workDir + "/dbpass.txt";
+		File passwordFile = new File(passwordPath);
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(passwordFile);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println("Could not find the password file! Make sure to use RunServer");
+			System.exit(1);
+		}
+		String thePassword = scanner.nextLine();
+		scanner.close();
+		this.dbPassword = thePassword;
+	}
 
-    private void connect() {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            conn = DriverManager.getConnection(DB_URL, USER, dbPassword);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	private void connect() {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			DriverManager.setLoginTimeout(10);
+			conn = DriverManager.getConnection(DB_URL, USER, dbPassword);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-    /**
-     * @param sql the SELECT query
-     * @return results of the query where columns content is all strings
-     */
-    public List<Map<String, String>> retrieveQueryString(String sql) {
-        return retrieveQueryString(sql, true);
-    }
+	/**
+	 * @param sql
+	 *            the SELECT query
+	 * @return results of the query where columns content is all strings
+	 */
+	public List<Map<String, String>> retrieveQueryString(String sql) {
+		return retrieveQueryString(sql, true);
+	}
 
-    // if `retry` is set, the database will try to re-establish
-    // a broken connection and query again. Otherwise, it will
-    // print the Exception to console
-    private List<Map<String, String>> retrieveQueryString(String sql, boolean retry) {
-        List<Map<String, String>> result = new ArrayList<>();
+	// if `retry` is set, the database will try to re-establish
+	// a broken connection and query again. Otherwise, it will
+	// print the Exception to console
+	private List<Map<String, String>> retrieveQueryString(String sql, boolean retry) {
+		List<Map<String, String>> result = new ArrayList<>();
 
-        try {
-            Statement stmt = conn.createStatement();
-            ResultSet rs = stmt.executeQuery(sql);
+		try {
+			Statement stmt = conn.createStatement();
+			ResultSet rs = stmt.executeQuery(sql);
 
-            Map<String, String> row;
-            ResultSetMetaData metaData = rs.getMetaData();
-            Integer columnCount = metaData.getColumnCount();
+			Map<String, String> row;
+			ResultSetMetaData metaData = rs.getMetaData();
+			Integer columnCount = metaData.getColumnCount();
 
-            while (rs.next()) {
-                row = new HashMap<>();
-                for (int i = 1; i <= columnCount; i++) {
-                    row.put(metaData.getColumnName(i), rs.getString(i));
-                }
-                result.add(row);
-            }
+			while (rs.next()) {
+				row = new HashMap<>();
+				for (int i = 1; i <= columnCount; i++) {
+					row.put(metaData.getColumnName(i), rs.getString(i));
+				}
+				result.add(row);
+			}
 
-            rs.close();
-            stmt.close();
-        } catch (Exception e) {
-            connect();
-            if (retry) {
-                return retrieveQueryString(sql, false);
-            } else {
-                e.printStackTrace();
-            }
-        }
+			rs.close();
+			stmt.close();
+		} catch (Exception e) {
+			connect();
+			if (retry) {
+				return retrieveQueryString(sql, false);
+			} else {
+				e.printStackTrace();
+			}
+		}
 
-        return result;
-    }
+		return result;
+	}
 
-    /**
-     * @param query the SELECT query
-     * @return results of the query
-     */
-    public List<Map<String, Object>> retrieveQuery(String query) {
-        return retrieveQuery(query, new String[0],true);
-    }
-    
-    public List<Map<String, Object>> retrieveStatement(String query, String[] parameters) {
-        return retrieveQuery(query, parameters, true);
-    }
+	/**
+	 * @param query
+	 *            the SELECT query
+	 * @return results of the query
+	 */
+	public List<Map<String, Object>> retrieveQuery(String query) {
+		return retrieveQuery(query, new String[0], true);
+	}
 
-    // if `retry` is set, the database will try to re-establish
-    // a broken connection and query again. Otherwise, it will
-    // print the Exception to console
-    private List<Map<String, Object>> retrieveQuery(String query, String[] parameters, boolean retry) {
-        List<Map<String, Object>> result = new ArrayList<>();
+	public List<Map<String, Object>> retrieveStatement(String query, String[] parameters) {
+		return retrieveQuery(query, parameters, true);
+	}
 
-        try {
-            PreparedStatement stmt = conn.prepareStatement(query);
-            for (int i = 0; i < parameters.length; i++) {
-                stmt.setString(i + 1, parameters[i]);
-            }
-            
-            ResultSet rs = stmt.executeQuery();
+	// if `retry` is set, the database will try to re-establish
+	// a broken connection and query again. Otherwise, it will
+	// print the Exception to console
+	private List<Map<String, Object>> retrieveQuery(String query, String[] parameters, boolean retry) {
+		List<Map<String, Object>> result = new ArrayList<>();
 
-            Map<String, Object> row;
-            ResultSetMetaData metaData = rs.getMetaData();
-            Integer columnCount = metaData.getColumnCount();
-            while (rs.next()) {
-                row = new HashMap<>();
-                for (int i = 1; i <= columnCount; i++) {
-                    row.put(metaData.getColumnName(i), rs.getObject(i));
-                }
-                result.add(row);
-            }
+		try {
+			PreparedStatement stmt = conn.prepareStatement(query);
+			for (int i = 0; i < parameters.length; i++) {
+				stmt.setString(i + 1, parameters[i]);
+			}
 
-            rs.close();
-            stmt.close();
-        } catch (Exception e) {
-            connect();
-            if (retry) {
-                return retrieveQuery(query, parameters, false);
-            } else {
-                e.printStackTrace();
-            }
-        }
-        return result;
-    }
-    
-    
+			ResultSet rs = stmt.executeQuery();
 
-    /**
-     * @param query the INSERT, UPDATE, or DELETE query
-     * @return number of rows returned
-     */
-    public int executeQuery(String query) {
-        return executeQuery(query, true);
-    }
+			Map<String, Object> row;
+			ResultSetMetaData metaData = rs.getMetaData();
+			Integer columnCount = metaData.getColumnCount();
+			while (rs.next()) {
+				row = new HashMap<>();
+				for (int i = 1; i <= columnCount; i++) {
+					row.put(metaData.getColumnName(i), rs.getObject(i));
+				}
+				result.add(row);
+			}
 
-    // if `retry` is set, the database will try to re-establish
-    // a broken connection and query again. Otherwise, it will
-    // print the Exception to console
-    private int executeQuery(String query, boolean retry) {
-        int result = 0;
-        try {
-            Statement stmt = conn.createStatement();
-            result = stmt.executeUpdate(query);
-            stmt.close();
-        } catch (Exception e) {
-            connect();
-            if (retry) {
-                return executeQuery(query, false);
-            } else {
-                e.printStackTrace();
-            }
-        }
-        return result;
-    }
+			rs.close();
+			stmt.close();
+		} catch (Exception e) {
+			connect();
+			if (retry) {
+				return retrieveQuery(query, parameters, false);
+			} else {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
 
-    /**
-     * @param query the INSERT, UPDATE, or DELETE query
-     * @param parameters to interpolate in statement
-     * @return number of rows returned
-     */
-    public int executeStatement(String query, String[] parameters) {
-        return executeStatement(query, parameters, true);
-    }
+	/**
+	 * @param query
+	 *            the INSERT, UPDATE, or DELETE query
+	 * @return number of rows returned
+	 */
+	public int executeQuery(String query) {
+		return executeQuery(query, true);
+	}
 
-    private int executeStatement(String query, String[] parameters, boolean retry) {
-        int result = 0;
-        try {
-            PreparedStatement stmt = conn.prepareStatement(query);
-            for (int i = 0; i < parameters.length; i++) {
-                stmt.setString(i + 1, parameters[i]);
-            }
-            result = stmt.executeUpdate();
-            stmt.close();
-        } catch (Exception e) {
-            connect();
-            if (retry) {
-                return executeStatement(query, parameters, false);
-            } else {
-                e.printStackTrace();
-            }
-        }
+	// if `retry` is set, the database will try to re-establish
+	// a broken connection and query again. Otherwise, it will
+	// print the Exception to console
+	private int executeQuery(String query, boolean retry) {
+		int result = 0;
+		try {
+			Statement stmt = conn.createStatement();
+			result = stmt.executeUpdate(query);
+			stmt.close();
+		} catch (Exception e) {
+			connect();
+			if (retry) {
+				return executeQuery(query, false);
+			} else {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
 
-        return result;
-    }
+	/**
+	 * @param query
+	 *            the INSERT, UPDATE, or DELETE query
+	 * @param parameters
+	 *            to interpolate in statement
+	 * @return number of rows returned
+	 */
+	public int executeStatement(String query, String[] parameters) {
+		return executeStatement(query, parameters, true);
+	}
+
+	private int executeStatement(String query, String[] parameters, boolean retry) {
+		int result = 0;
+		try {
+			PreparedStatement stmt = conn.prepareStatement(query);
+			for (int i = 0; i < parameters.length; i++) {
+				stmt.setString(i + 1, parameters[i]);
+			}
+			result = stmt.executeUpdate();
+			stmt.close();
+		} catch (Exception e) {
+			connect();
+			if (retry) {
+				return executeStatement(query, parameters, false);
+			} else {
+				e.printStackTrace();
+			}
+		}
+
+		return result;
+	}
 
 }
