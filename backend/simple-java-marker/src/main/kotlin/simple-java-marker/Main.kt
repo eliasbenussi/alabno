309910e66fc48045b5442e115182c5b4d676dcf2
eb1314 <eb1314@ic.ac.uk>
2016-11-28 12:06:07 +0000

    simple-java-marker: antlr setup done
    Signed-off-by: eb1314 <eb1314@ic.ac.uk>

---
+++ backend/simple-java-marker/src/main/kotlin/simple-java-marker/Main.kt
@@ -0,0 +1,36 @@
+import java_antlr.Java8Lexer
+import java_antlr.Java8Parser
+import json_parser.MicroServiceInputParser
+import org.antlr.v4.runtime.ANTLRInputStream
+import org.antlr.v4.runtime.CommonTokenStream
+import java.io.File
+import java.io.FileInputStream
+
+fun main(args: Array<String>) {
+
+    if (args.size < 2) {
+        throw IllegalArgumentException("Not enough arguments")
+    }
+    val inputParser = MicroServiceInputParser.parseFile(File(args[0]))
+    val language = inputParser.language
+    when {
+        language.contains("java") -> {
+            javaANTLRRunner(inputParser.path)
+        }
+        else -> throw IllegalArgumentException("Not so hello world")
+    }
+}
+
+fun javaANTLRRunner(path: String) {
+    val file = File(path)
+    val files = file.listFiles().filter { it.endsWith(".java") }
+    files
+            .map { ANTLRInputStream(FileInputStream(it)) }
+            .map { CommonTokenStream(Java8Lexer(it)) }
+            .map { Java8Parser(it) }
+            .forEach {
+                println(it.additionalBound())
+                //def visit = new ASTGenerator()
+                //visit.visit(fileParser.primary())
+            }
+}
