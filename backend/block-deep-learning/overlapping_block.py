765b41743a0a59f7d37c33da62bf4ba29928aa4e
Ivan Procaccini <ip914@ic.ac.uk>
2016-11-28 23:23:23 +0000

    algorithm rework with Giulio

---
+++ backend/block-deep-learning/overlapping_block.py
@@ -0,0 +1,74 @@
+import os
+import sys
+
+class Block:
+    
+    def __init__(self, lineno):
+        self.lineno = lineno
+        self.content = ''
+
+    def __str__(self):
+        return '[{},{}]'.format(self.lineno, self.content)
+
+# Split source in overlapping blocks
+# ensuring lines of same length
+class Script_Blocks_Container:
+        
+    def __init__(self, source_p):
+        self.source_p = source_p
+
+        # Max size of the block in characters
+        self.container = []
+
+    def get_container(self):
+        return self.container
+
+    def split(self):
+        BLOCK_SIZE = 200
+        BLOCK_OFFSET = 50
+
+        source_file = open(self.source_p, 'r')
+        inverse_map = {}
+        char_index = 0
+        line_index = 1
+        for line in source_file:
+            for c in line:
+                inverse_map[char_index] = line_index
+                char_index += 1
+            line_index += 1
+        
+        source_file.close()
+        source_file = open(self.source_p, 'r')
+        source_text = source_file.read()
+
+        all_blocks = []
+        i = 0
+        while i < len(source_text):
+            j = 0
+            current_block = Block(inverse_map[i])
+            while j < BLOCK_SIZE and i < len(source_text):
+                current_block.content += source_text[i]
+                i += 1
+                j += 1
+            all_blocks.append(current_block)
+            if i >= len(source_text):
+                break
+            i -= BLOCK_SIZE - BLOCK_OFFSET
+
+        # TODO last few blocks might not be long enough, add whitespaces to them
+        
+        self.container = all_blocks
+
+
+
+
+block_container = Script_Blocks_Container("./test.txt")
+block_container.split()
+_container = block_container.get_container()
+
+for c in _container:
+  print(c.__str__())
+                         
+
+
+
