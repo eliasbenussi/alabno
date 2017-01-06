15210c411e6dbc17c2aca61cef70cca30dbe2f44
Ivan Procaccini <ip914@ic.ac.uk>
2017-01-06 21:07:09 +0100

    refactoring + inclusion of overblockmarker to microservices.py

--- infrastructure/microservices.py
+++ infrastructure/microservices.py
@@ -7,6 +7,7 @@ microservices = [
     {"name": "compiler", "location": "java -jar backend/compile-checker/target/compile-checker-1.0-SNAPSHOT-jar-with-dependencies.jar"},
     {"name": "linter", "location": "java -jar backend/linter/target/linter-1.0-SNAPSHOT-jar-with-dependencies.jar" },
     {"name": "simplehaskellmarker", "location": "backend/simple-haskell-marker/SimpleHaskellMarker"},
+    {"name": "overblockmarker", "location": "backend/overlapping-block-marker/OverlappingBlockMarker"},
     {"name": "markmarker", "location": "java -jar backend/mark_marker/target/mark_marker-1.0-SNAPSHOT-jar-with-dependencies.jar"},
     {"name": "companal", "location": "java -jar backend/complexity_analyser/target/complexity_analyser-1.0-SNAPSHOT-jar-with-dependencies.jar"}
 ]
