33e92ff47be758bb5243e979c2bb4e2fe58c7763
giuliojiang <giuliojiang@gmail.com>
2017-01-14 15:04:14 +0000

    normalize training and test data

--- backend/overlapping-block-marker/Classifier.py
+++ backend/overlapping-block-marker/Classifier.py
@@ -1,6 +1,7 @@
 import ast
 import sys
 from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
 
 class Classifier:
     
@@ -17,19 +18,28 @@ class Classifier:
             self.X.append(float_formatted)
             self.y.append(cat_numb)
         
-        
         # Initialize the classifier (stochastic gradient-descent)
-        self.clf = MLPClassifier(solver='adam', alpha=1e-5, 
-                                   hidden_layer_sizes=(5, 2), random_state=1)
+        self.clf = MLPClassifier(activation='relu', alpha=1e-05, batch_size='auto',
+                        beta_1=0.9, beta_2=0.999, early_stopping=False,
+                        epsilon=1e-08, hidden_layer_sizes=(5, 2), learning_rate='constant',
+                        learning_rate_init=0.001, max_iter=200, momentum=0.9,
+                        nesterovs_momentum=True, power_t=0.5, random_state=1, shuffle=True,
+                        solver='lbfgs', tol=0.0001, validation_fraction=0.1, verbose=False,
+                        warm_start=False)
                 
         try: 
-            self.clf.fit(self.X, self.y)
+            scaler = StandardScaler()
+            scaler.fit(self.X)
+            transformed_X = scaler.transform(self.X)
+            self.clf.fit(transformed_X, self.y)
+            self.scaler = scaler
         except Exception as e:
             print (str(e))
             sys.exit(1)
 
     def predict(self, new_data):
-        prediction = self.clf.predict(new_data)
+        transformed_data = self.scaler.transform(new_data)
+        prediction = self.clf.predict(transformed_data)
         if len(prediction) != len(new_data):
             raise Exception('[Classifier] Classification failed. Aborting.')
         return prediction
